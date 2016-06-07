f0d35423eb21faa9991fc99070aa8b45c601be34
Rahiel Kasim <rahielkasim@gmail.com>
2016-06-07 11:55:32 +0200

    Bismillah

---
+++ README.md
@@ -0,0 +1,2 @@
+My old Emacs configuration, I used this before I switched over to Spacemacs.
+Here for archiving purposes.
---
+++ init.el
@@ -0,0 +1,12 @@
+(org-babel-load-file "~/.emacs.d/rahiel.org")
+(put 'downcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults))))
+
+(put 'upcase-region 'disabled nil)
